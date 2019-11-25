15fab8021f45b0f1dae21fe0c71ed46a77585c83
nickalai <nicklai802@gmail.com>
2019-11-25 15:58:26 -0800

    Added pause and menu scripts

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public string levelToLoad;
+    void Start()
+    {
+  
+    }
+
+    // Start is called before the first frame update
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // Starts the game by loading the first level
+    public void Play()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,81 @@
+﻿/*
+Name: Nick Lai
+Student ID#: 2282417
+Chapman email: lai137@mail.chapman.edu
+Course Number and Section: 340-02
+Assignment: Aim Rush
+Contains logic for the pause menu and end screens
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Grabs DDOL values on Awake
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame. Checks if the game is paused or not
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+
+            else if (!isPaused)
+            {
+                Pause();
+            }
+        }
+    }
+
+    // Contains logic for pausing the game
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    // Contains logic for resuming the game
+    public void Resume()
+    {
+        //sm.PlaySound(sm.buttonClick, 1f);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    // Contains logic for returning to the Main menu
+    public void BackToMainMenu()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
