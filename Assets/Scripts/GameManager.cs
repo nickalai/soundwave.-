404f9b93b4e681e2863f18680768b41d9d4eecab
nickalai <nicklai802@gmail.com>
2019-12-03 02:36:50 -0800

    Added Koreography assets. Rewrote scripts from Rhythm Game Demo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,225 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class GameManager : MonoBehaviour
+{
+    #region Variables
+    [Tooltip("The EventID of the track to use for target generation.")]
+    [EventID]
+    public string eventID;
+
+    [Tooltip("The number of milliseconds (both early and late) within which input will be detected as a hit.")]
+    [Range(8f, 150f)]
+    public float hitWindowRangeInMS = 80f;
+
+    [Tooltip("The number of units traversed per seocond by Notes.")]
+    public float noteSpeed = 1f;
+
+    [Tooltip("The archetype to use for generating notes. Can be a prefab.")]
+    public Notes noteObjectArchetype;
+
+    [Tooltip("The list of LaneManager objects that represent a lane for an event to travel down.")]
+    public List<LaneManager> noteLanes = new List<LaneManager>();
+
+    [Tooltip("The amount of time in seconds to provide before playback of the audio begins. Changes to this value are not immediately handled during the lead-in phase while playing in the editor.")]
+    public float leadInTime;
+
+    [Tooltip("The AudioSource through which the Koreographed audio will be played. Be sure to disable 'Play on Awake' in the Music Player.")]
+    public AudioSource aus;
+
+    // The amount of lead-in time left before the audio is audible.
+    float leadInTimeLeft;
+
+    // The amount of time left before audio is played (handles Event Delay).
+    float timeLeftToPlay;
+
+    // Local cache of the Koreography loaded into the Koreographer component.
+    Koreography playingKoreo;
+
+    // Sample range within which a viable event may be hit.
+    int hitWindowRangeInSamples;
+
+    // Pool for containing Notes to reduce unnecessary Instatiation/Destruction.
+    Stack<Notes> notePool = new Stack<Notes>();
+
+    #endregion
+    #region Return Statements
+    // Public access to the hit window
+    public int HitWindowSampleWidth
+    {
+        get
+        {
+            return hitWindowRangeInSamples;
+        }
+    }
+
+    // Access to the current hit window size in Unity units.
+    public float WindowSizeInUnits
+    {
+        get
+        {
+            return noteSpeed * (hitWindowRangeInMS * 0.001f);
+        }
+    }
+
+    // Sample Rate specified by Koreography
+    public int SampleRate
+    {
+        get
+        {
+            return playingKoreo.SampleRate;
+        }
+    }
+
+    // Current sample time, including any necessary delays.
+    public int DelayedSampleTime
+    {
+        get
+        {
+            return playingKoreo.GetLatestSampleTime() - (int)(aus.pitch * leadInTimeLeft * SampleRate);
+        }
+    }
+
+    #endregion
+    #region Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeLeadIn();
+
+        // Init all Lanes.
+        for(int i = 0; i < noteLanes.Count; ++i)
+        {
+            noteLanes[i].Initialize(this);
+        }
+
+        // Init events.
+        playingKoreo = Koreographer.Instance.GetKoreographyAtIndex(0);
+
+        // Grab all events from Koreography.
+        KoreographyTrackBase rhythmTrack = playingKoreo.GetTrackByID(eventID);
+        List<KoreographyEvent> rawEvents = rhythmTrack.GetAllEvents();
+
+        for(int i = 0; i < rawEvents.Count; ++i)
+        {
+            KoreographyEvent evt = rawEvents[i];
+            string payload = evt.GetTextValue();
+
+            // Finds correct Lane.
+            for (int j = 0; j < noteLanes.Count; ++j)
+            {
+                LaneManager lane = noteLanes[j];
+                if (lane.DoesMatchPayload(payload))
+                {
+                    // Adds object for input tracking.
+                    lane.AddEventToLane(evt);
+                    break;
+                }
+            }
+        }
+    }
+
+    // Sets up the lead-in time. Begins audio playback immediately if the specified lead-in time is 0.
+    void InitializeLeadIn()
+    {
+        // Inits lead-in time only if specified.
+        if (leadInTime > 0f)
+        {
+            // Sets up to delay the beginning of playback.
+            leadInTimeLeft = leadInTime;
+            timeLeftToPlay = leadInTime - Koreographer.Instance.EventDelayInSeconds;
+        }
+
+        else
+        {
+            aus.time = -leadInTime;
+            aus.Play();
+        }
+    }
+
+    // Update is called once per frame.
+    void Update()
+    {
+        UpdateInternalValues();
+        // Countdown some of lead-in time.
+        if(leadInTimeLeft > 0f)
+        {
+            leadInTimeLeft = Mathf.Max(leadInTimeLeft - Time.unscaledDeltaTime, 0f);
+        }
+
+        // Countdown th time left to play if necessary.
+        if(timeLeftToPlay > 0f)
+        {
+            timeLeftToPlay -= Time.unscaledDeltaTime;
+            // Checks if it is time to begin playback.
+            if(timeLeftToPlay <= 0f)
+            {
+                aus.time = -timeLeftToPlay;
+                aus.Play();
+
+                timeLeftToPlay = 0f;
+            }
+        }
+    }
+
+    // Update any internal values that depend on externally accessible fields (public/inspector).
+    void UpdateInternalValues()
+    {
+        hitWindowRangeInSamples = (int)(0.001f * hitWindowRangeInMS * SampleRate);
+    }
+
+    // Gets a freshly activated Note from the pool.
+    public Notes GetFreshNote()
+    {
+        Notes retObj;
+
+        if(notePool.Count > 0)
+        {
+            retObj = notePool.Pop();
+        }
+        else
+        {
+            retObj = GameObject.Instantiate<Notes>(noteObjectArchetype);
+        }
+
+        retObj.gameObject.SetActive(true);
+        retObj.enabled = true;
+
+        return retObj;
+    }
+
+    // Deactivates and returns a Note to the pool.
+    public void ReturnNoteToPool(Notes note)
+    {
+        if (note != null)
+        {
+            note.enabled = false;
+            note.gameObject.SetActive(false);
+            notePool.Push(note);
+        }
+    }
+
+    // Restarts the game, causing all Lanes and any active Notes to reset/clear.
+    public void Restart()
+    {
+        // Resets audio
+        aus.Stop();
+        aus.time = 0f;
+
+        // Flushes the queue of delayed event updates. Resets Koreography and ensures delayed events that haven't been sent yet don't continue to be sent.
+        Koreographer.Instance.FlushDelayQueue(playingKoreo);
+        // Resets Koreography time.
+        playingKoreo.ResetTimings();
+        // Resets lanes so that tracking starts over.
+        for (int i = 0; i < noteLanes.Count; ++i)
+        {
+            noteLanes[i].Restart();
+        }
+        //Reinit the lead-in timing.
+        InitializeLeadIn();
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/LaneManager.cs
@@ -0,0 +1,224 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class LaneManager : MonoBehaviour
+{
+    #region Variables
+    [Tooltip("A reference to the visuals for the \"target\" location.")]
+    public GameObject targetVisuals;
+
+    [Tooltip("The keyboard button used by this lane.")]
+    public KeyCode keyboardButton;
+
+    [Tooltip("The controller button used by this lane [NOT CURRENTLY USED].")]
+    public string controllerButton;
+
+    [Tooltip("A list of Payload strings that Koreography Events will contain for this lane.")]
+    public List<string> matchedPayloads = new List<string>();
+
+    // List that contains all events for a lane. Added by the GameManager.
+    List<KoreographyEvent> laneEvents = new List<KoreographyEvent>();
+
+    // Queue that contains all Notes currently on screen within this lane.
+    Queue<Notes> trackedNotes = new Queue<Notes>();
+
+    // Reference to the GameManager. Provides access to the Notes pool and other parameters.
+    GameManager gm;
+
+    // Lifetime boundaries.
+    float spawnZ = 0f;
+    float despawnZ = 0f;
+
+    // Index of the next event to check for spawn timing in this lane.
+    int pendingEventIndex = 0;
+
+    // Feedback Scales used for resizing buttons on press.
+    Vector3 defaultScale;
+    float scaleNormal = 1f;
+    float scalePress = 1.4f;
+    float scaleHold = 1.2f;
+
+    #endregion
+    #region Return Statements
+    // Position at which new Notes should spawn
+    public Vector3 SpawnPosition
+    {
+        get
+        {
+            return new Vector3(transform.position.x, transform.position.y, spawnZ);
+        }
+    }
+
+    // The position at which the timing target exists.
+    public Vector3 TargetPosition
+    {
+        get
+        {
+            return new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+
+    // The position at which Notes should despawn and return to the pool.
+    public float DespawnZ
+    {
+        get
+        {
+            return despawnZ;
+        }
+    }
+    #endregion
+    #region Methods
+
+    public void Initialize(GameManager manager)
+    {
+        gm = manager;
+    }
+
+    // Controls cleanup, resets internals to a fresh state.
+    public void Restart(int newSampleTime = 0)
+    {
+        // Finds the index of the first event at or beyond the target sample time.
+        for (int i = 0; i < laneEvents.Count; ++i)
+        {
+            if(laneEvents[i].StartSample >= newSampleTime)
+            {
+                pendingEventIndex = i;
+                break;
+            }
+        }
+        // Clears out tracked notes.
+        int numToClear = trackedNotes.Count;
+        for (int i = 0; i < numToClear; ++i)
+        {
+            trackedNotes.Dequeue().OnClear();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Gets the vertical bounds of the camera. Offset by a bit to allow for offscrean spawning/removal
+        float cameraOffsetZ = -Camera.main.transform.position.z;
+        spawnZ = Camera.main.ViewportToWorldPoint(new Vector3(0f, 1f, cameraOffsetZ)).y + 1f;
+        despawnZ = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, cameraOffsetZ)).z - 1f;
+
+        defaultScale = targetVisuals.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Clears out invalid entries.
+        while (trackedNotes.Count > 0 && trackedNotes.Peek().IsNoteMissed())
+        {
+            trackedNotes.Dequeue();
+        }
+        // Checks for new spawns.
+        CheckSpawnNext();
+        // Checks for input
+        if (Input.GetKeyDown(keyboardButton))
+        {
+            CheckNoteHit();
+            SetScalePress();
+        }
+        else if (Input.GetKey(keyboardButton))
+        {
+            SetScaleHold();
+        }
+        else if (Input.GetKeyUp(keyboardButton))
+        {
+            SetScaleDefault();
+        }
+    }
+
+    // Adjusts the scale with a multiplier against default scale.
+    void AdjustScale(float multiplier)
+    {
+        targetVisuals.transform.localScale = defaultScale * multiplier;
+    }
+
+    // Uses the Target position and the current Note speed to determine audio sample "position" of the spawn location. Relative to the audio sample position at the "now" time.
+    int GetSpawnSampleOffset()
+    {
+        // Determines the sample offset at the current speed.
+        float spawnDistToTarget = spawnZ - transform.position.z;
+        // Determines time to location at the current speed.
+        double spawnSecsToTarget = (double)spawnDistToTarget / (double)gm.noteSpeed;
+        // Returns samples to the target.
+        return (int)(spawnSecsToTarget * gm.SampleRate);
+    }
+
+    // Checks if a Note is hit. If hit, will perform the Hit and remove the object from trackedNotes.
+    public void CheckNoteHit()
+    {
+        // Always check only the first event as we clear out missed entries before.
+        if (trackedNotes.Count > 0 && trackedNotes.Peek().IsNoteHittable())
+        {
+            Notes hitNote = trackedNotes.Dequeue();
+            hitNote.OnHit();
+        }
+    }
+
+    // Checks if the next Note should be spawned. If true, spawns the Note and adds it to trackedNotes.
+    void CheckSpawnNext()
+    {
+        int samplesToTarget = GetSpawnSampleOffset();
+        int currentTime = gm.DelayedSampleTime;
+        // Spawn for all events within range.
+        while (pendingEventIndex < laneEvents.Count && laneEvents[pendingEventIndex].StartSample < currentTime + samplesToTarget)
+        {
+            KoreographyEvent evt = laneEvents[pendingEventIndex];
+
+            Notes freshNote = gm.GetFreshNote();
+            freshNote.InitializeNote(evt, this, gm);
+
+            trackedNotes.Enqueue(freshNote);
+
+            pendingEventIndex++;
+        }
+    }
+
+    // Adds Koreography event to the Lane. Contains the timing information that defines when a Note should appear on screen.
+    public void AddEventToLane(KoreographyEvent evt)
+    {
+        laneEvents.Add(evt);
+    }
+
+    // Checks to see if the string value passed in matches any of the configured values specified in the matchedPayloads list.
+    public bool DoesMatchPayload(string payload)
+    {
+        bool isMatched = false;
+
+        for (int i = 0; i < matchedPayloads.Count; ++i)
+        {
+            if (payload == matchedPayloads[i])
+            {
+                isMatched = true;
+                break;
+            }
+        }
+
+        return isMatched;
+    }
+
+    // Sets the Target scale to the original default scale.
+    public void SetScaleDefault()
+    {
+        AdjustScale(scaleNormal);
+    }
+
+    // Sets the Target scale to the specified "initially pressed" scale.
+    public void SetScalePress()
+    {
+        AdjustScale(scalePress);
+    }
+
+    // SEts the Target scale to the specified "continuously held" scale.
+    public void SetScaleHold()
+    {
+        AdjustScale(scaleHold);
+    }
+    #endregion
+}
