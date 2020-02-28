fdeb59da7f1cdf4c1d6627c0543ee5dc58a3565b
nickalai <nicklai802@gmail.com>
2020-02-28 01:23:33 -0800

    Added a speed slider for demoing use. Fixed weird rotation issue on notes and incorrect color for notes

---
+++ Assets/Scripts/NoteSpeedSlider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoteSpeedSlider : MonoBehaviour
+{
+    [Tooltip("The Text Component that will display the Speed number.")]
+    public Text readoutText;
+
+    [Tooltip("The Slider that controls the Pitch number.")]
+    public Slider slider;
+
+    public GameManager gm;
+
+    void Awake()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float speed = gm.noteSpeed;
+        slider.value = speed;
+        SetNewNoteSpeed(slider.value);
+    }
+
+    public void SetNewNoteSpeed(float newNoteSpeed)
+    {
+        gm.noteSpeed = newNoteSpeed;
+        readoutText.text = newNoteSpeed.ToString("0.####") + "x";
+    }
+}
