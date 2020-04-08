61ffe4955f9901f27195b7231ad978b65eeb3d43
nickalai <nicklai802@gmail.com>
2020-04-07 19:37:37 -0700

    Added ScriptableObjects to represent charts and began working on Song Select functionality

---
+++ Assets/Scripts/ChartDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChartDisplay : MonoBehaviour
+{
+    public Chart chart;
+    public Text chartName;
+    public Text chartDifficulty;
+    public Text chartDifficultyNum;
+    public Text chartHighScore;
+    public GameObject completedChart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayChart();
+    }
+
+    public void DisplayChart()
+    {
+        chartName.text = chart.name;
+        chartDifficulty.text = chart.difficulty;
+        chartDifficultyNum.text = chart.difficultyNum.ToString();
+        chartHighScore.text = chart.highScore.ToString();
+        if (chart.completed == true)
+        {
+            completedChart.SetActive(true);
+        }
+
+    }
+}
---
+++ Assets/Scripts/LoadChart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class LoadChart : MonoBehaviour
+{
+    public static LoadChart instance;
+    public Koreography chartToLoad;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
