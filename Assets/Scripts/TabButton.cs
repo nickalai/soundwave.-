5ac5973a1fb8fd29194061597ec76ca1c095e7cd
Cheryl <cherleelynn@gmail.ccom>
2020-04-08 09:15:08 -0700

    added UI visual elements, tab system works with a time delay now, rotation works but needs a lerp

---
+++ Assets/Scripts/TabButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class TabButton : MonoBehaviour
+{
+    public TabGroup tabGroup;
+    public int songID;
+    public UnityEvent onTabSelected;
+    public UnityEvent onTabDeselected;
+    public TabSelect tabSelect;
+
+    //public Image background;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tabGroup.Subscribe(this);
+    }
+
+    void Update() {
+        if (tabSelect.currentTab == songID) {
+            tabGroup.OnTabSelected(this);
+        }
+    }
+
+    public void Select() {
+        if (onTabSelected != null) {
+            onTabSelected.Invoke();
+        }
+    }
+
+    public void Deselect() {
+        if (onTabDeselected != null) {
+            onTabDeselected.Invoke();
+        }
+    }
+}
