6dd88fd5864a7c99e527a9d372e6263751b78dcd
Cheryl <cherleelynn@gmail.ccom>
2020-04-08 09:15:08 -0700

    added UI visual elements, tab system works with a time delay now, rotation works but needs a lerp

---
+++ Assets/Scripts/TabGroup.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<TabButton> tabButtons;
+    public TabButton selectedTab;
+    public List<GameObject> objectsToSwap;
+    public TabSelect tabSelect;
+
+    //change sprites
+    public Sprite tabActive;
+    public Sprite tabIdle;
+
+    public void Subscribe(TabButton button) {
+        if(tabButtons == null) { 
+            tabButtons = new List<TabButton>();
+        }
+
+        tabButtons.Add(button);
+        tabButtons.Sort((e1, e2) => e1.transform.GetSiblingIndex().CompareTo(e2.transform.GetSiblingIndex()));
+    }
+
+    //these change the state of the tabs
+    
+    public void OnTabEnter(TabButton button) {
+
+    }
+
+    public void OnTabExit(TabButton button) {
+
+    }
+
+    public void OnTabSelected(TabButton button) {
+
+        if(selectedTab != null) {
+            selectedTab.Deselect();
+        }
+
+        //assign selected tab 
+        selectedTab = button;
+
+        selectedTab.Select();
+
+        //order of objects in this list should match the button order
+        int index = button.transform.GetSiblingIndex();
+
+        //iterate through game objects
+        for (int i=0; i < objectsToSwap.Count; i++) {
+            if (i == index) {
+                objectsToSwap[i].SetActive(true);
+            }
+            else {
+                objectsToSwap[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ResetTabs() {
+
+    }
+
+}
---
+++ Assets/Scripts/TabSelect.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class TabSelect : MonoBehaviour
+{
+    public int currentTab;
+    public TabGroup tabGroup;
+    private float zAngle;
+    private float targetTime;
+    //public UnityEvent ShiftRight;
+    //public UnityEvent ShiftLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //default tab position is 0;
+        currentTab = 0;
+        targetTime = 1.0f;
+
+        InvokeRepeating("getInput", 1.0f, targetTime);
+    }
+
+    void Update() {
+        
+        if (Input.GetAxis("Horizontal") < 1 && Input.GetAxis("Horizontal") > -1) {
+            targetTime = 1.0f;
+        }
+        else if (Input.GetAxis("Horizontal") == 1 || Input.GetAxis("Horizontal") == -1) {
+            targetTime = targetTime - 0.5f;
+        }
+
+        //getInput();
+        
+
+        Debug.Log("Input: " + Input.GetAxis("Horizontal"));
+        //Debug.Log("Frame Rate: " + Application.targetFrameRate);
+    }
+
+    void getInput() {
+        //going to the right, positive
+        if (Input.GetAxis("Horizontal") > 0) {
+            ShiftRight();
+            RotateRight();
+        }
+        //going to the left, negative
+        else if (Input.GetAxis("Horizontal") < 0) {
+            ShiftLeft();
+            RotateLeft();
+        }
+    }
+
+    public void ShiftRight() {
+        //increment by 1 
+        if (currentTab >= 0 && currentTab < tabGroup.tabButtons.Count) {
+            currentTab++;
+        }
+        Debug.Log(currentTab);
+    }
+
+    public void ShiftLeft() {
+        //decrement by 1 
+        if (currentTab > 0 && currentTab <= tabGroup.tabButtons.Count) {
+            currentTab--;
+        }
+        Debug.Log(currentTab);
+    }
+
+    public void RotateRight() {
+        zAngle = (float)(360 / tabGroup.tabButtons.Count) * currentTab;
+        this.transform.Rotate(0.0f, 0.0f, zAngle, Space.Self);
+    }
+
+    public void RotateLeft() {
+        zAngle = (float)(360 / tabGroup.tabButtons.Count) * currentTab;
+        this.transform.Rotate(0.0f, 0.0f, zAngle, Space.Self);
+    }
+
+}
