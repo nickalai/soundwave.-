cd5fcba8925127483f35424cae01a07b200b35be
Cheryl <cherleelynn@gmail.ccom>
2020-04-08 21:38:02 -0700

    main menu animations added, some rotation added, menu has about 40% functionality right now

---
+++ Assets/Scripts/AnimCheck.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimCheck : MonoBehaviour
+{
+
+    public Animation anim;
+    public TabSelect tabSelect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (anim.isPlaying) {
+            tabSelect.Loading = true;
+        }
+        else if (!anim.isPlaying) {
+            tabSelect.Loading = false;
+        }
+    }
+}
--- Assets/Scripts/TabSelect.cs
+++ Assets/Scripts/TabSelect.cs
@@ -3,52 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class TabSelect : MonoBehaviour
 {
     public int currentTab;
     public TabGroup tabGroup;
-    private float zAngle;
-    private float targetTime;
-    //public UnityEvent ShiftRight;
-    //public UnityEvent ShiftLeft;
+    //public Animation anim;
+
+    //private int angle = 0;
+    List<Quaternion> rotList = new List<Quaternion>();
+    private float targetAngle = 0;
+    private float angleAmount;
+    public bool Loading = false;
+
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         //default tab position is 0;
         currentTab = 0;
-        targetTime = 1.0f;
 
-        InvokeRepeating("getInput", 1.0f, targetTime);
+        angleAmount = 360 / tabGroup.objectsToSwap.Count;
+        //Debug.Log(angleAmount);
+        //Debug.Log(tabGroup.objectsToSwap.Count);
+
+        CreateQuatList();
+        
+        //StartCoroutine(GetInput());
     }
 
-    void Update() {
-        
-        if (Input.GetAxis("Horizontal") < 1 && Input.GetAxis("Horizontal") > -1) {
-            targetTime = 1.0f;
-        }
-        else if (Input.GetAxis("Horizontal") == 1 || Input.GetAxis("Horizontal") == -1) {
-            targetTime = targetTime - 0.5f;
-        }
+    private void Update() {
 
-        //getInput();
-        
+        /*
+        if (anim.isPlaying) {
+            Loading = true;
+        }
+        else if (!anim.isPlaying) {
+            Loading = false;
+        }
+        */
 
-        Debug.Log("Input: " + Input.GetAxis("Horizontal"));
-        //Debug.Log("Frame Rate: " + Application.targetFrameRate);
+        if (Loading == false) {
+            GetInput();
+        }
+        
     }
 
-    void getInput() {
+    public void CreateQuatList() {
+        for (int i = 0; i < tabGroup.tabButtons.Count; i++) {
+            rotList.Add(Quaternion.Euler(new Vector3(0, 0, targetAngle)));
+            targetAngle = angleAmount*i;
+            //Debug.Log(rotList[i]);
+        }
+    }
+
+    public void GetInput() {
         //going to the right, positive
-        if (Input.GetAxis("Horizontal") > 0) {
+        //Debug.Log("Input: " + Input.GetAxis("Horizontal"));
+
+        if (Input.GetAxis("Horizontal") == 1) {
             ShiftRight();
-            RotateRight();
+            Rotate();
         }
         //going to the left, negative
-        else if (Input.GetAxis("Horizontal") < 0) {
+        else if (Input.GetAxis("Horizontal") == -1) {
             ShiftLeft();
-            RotateLeft();
-        }
+            Rotate();
+        }   
     }
 
     public void ShiftRight() {
@@ -56,7 +77,7 @@ public class TabSelect : MonoBehaviour
         if (currentTab >= 0 && currentTab < tabGroup.tabButtons.Count) {
             currentTab++;
         }
-        Debug.Log(currentTab);
+        //Debug.Log(currentTab);
     }
 
     public void ShiftLeft() {
@@ -64,17 +85,23 @@ public class TabSelect : MonoBehaviour
         if (currentTab > 0 && currentTab <= tabGroup.tabButtons.Count) {
             currentTab--;
         }
-        Debug.Log(currentTab);
+       // Debug.Log(currentTab);
     }
 
-    public void RotateRight() {
-        zAngle = (float)(360 / tabGroup.tabButtons.Count) * currentTab;
-        this.transform.Rotate(0.0f, 0.0f, zAngle, Space.Self);
+    public void Rotate() {
+        if (currentTab > 0 && currentTab < tabGroup.tabButtons.Count) {
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rotList[currentTab], 0.2f);
+        }
+        //Debug.Log(rotList[currentTab]);
     }
 
-    public void RotateLeft() {
-        zAngle = (float)(360 / tabGroup.tabButtons.Count) * currentTab;
-        this.transform.Rotate(0.0f, 0.0f, zAngle, Space.Self);
+    /*
+    IEnumerator Hold() {
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        yield return new WaitForSeconds(2);
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
+    */
+   
 
 }
