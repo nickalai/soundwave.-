1e276e41db517557d804a54022bbf9ef59d2db50
Cheryl <cherleelynn@gmail.ccom>
2020-04-14 04:08:02 -0700

    song selections scene fixed, main menu layout

--- Assets/Scripts/AnimCheck.cs
+++ Assets/Scripts/AnimCheck.cs
@@ -6,21 +6,13 @@ public class AnimCheck : MonoBehaviour
 {
 
     public Animation anim;
-    public TabSelect tabSelect;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animation>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (anim.isPlaying) {
-            tabSelect.Loading = true;
-        }
-        else if (!anim.isPlaying) {
-            tabSelect.Loading = false;
-        }
+    public bool CheckPlaying() {
+        return anim.isPlaying;
     }
 }
--- Assets/Scripts/TabSelect.cs
+++ Assets/Scripts/TabSelect.cs
@@ -9,95 +9,74 @@ public class TabSelect : MonoBehaviour
 {
     public int currentTab;
     public TabGroup tabGroup;
+    public int tabs;
 
-    List<Quaternion> rotList = new List<Quaternion>();
+    public List<Quaternion> rotList = new List<Quaternion>();
+
     private float targetAngle = 0;
     private float angleAmount;
-    public bool Loading = false;
+    public bool loading = false;
+
+    public AnimCheck animCheck;
 
 
     // Start is called before the first frame update
     void Start(){
         //default tab position is 0;
         currentTab = 0;
+        tabs = tabGroup.objectsToSwap.Count;
+        angleAmount = 360 / tabs;
 
-        angleAmount = 360 / tabGroup.objectsToSwap.Count;
-        //Debug.Log(angleAmount);
-        //Debug.Log(tabGroup.objectsToSwap.Count);
+        Debug.Log("current Tab = " + currentTab);
 
         CreateQuatList();
-        
-        //StartCoroutine(GetInput());
     }
 
     private void Update() {
+       // Debug.Log(Input.GetAxis("Horizontal"));
 
-        /*
-        if (anim.isPlaying) {
-            Loading = true;
+        if (Input.GetAxis("Horizontal") < 0.05 && Input.GetAxis("Horizontal") > -0.05 ) {
+            GetInput();
         }
-        else if (!anim.isPlaying) {
-            Loading = false;
-        }
-        */
-
-        GetInput();
-        
     }
 
-    public void CreateQuatList() {
-        for (int i = 0; i < tabGroup.tabButtons.Count; i++) {
+    void CreateQuatList() {
+        for (int i = 0; i < tabs; ++i) {
+            targetAngle = angleAmount * i;
+            //Debug.Log(targetAngle);
             rotList.Add(Quaternion.Euler(new Vector3(0, 0, targetAngle)));
-            targetAngle = angleAmount*i;
-            //Debug.Log(rotList[i]);
         }
     }
 
     public void GetInput() {
+        loading = true;
         //going to the right, positive
-        //Debug.Log("Input: " + Input.GetAxis("Horizontal"));
-
-        if (Input.GetAxis("Horizontal") == 1) {
+        if (Input.GetAxis("Horizontal")  > 0) {
             ShiftRight();
-            Rotate();
         }
         //going to the left, negative
-        else if (Input.GetAxis("Horizontal") == -1) {
+        else if (Input.GetAxis("Horizontal") < 0) {
             ShiftLeft();
-            Rotate();
         }   
     }
 
     public void ShiftRight() {
         //increment by 1 
-        if (currentTab >= 0 && currentTab < tabGroup.tabButtons.Count) {
+        int prevTab = currentTab;
+        if (currentTab > -1 && currentTab < tabs -1) {
             currentTab++;
+            //transform.rotation = Quaternion.Slerp(rotList[prevTab], rotList[currentTab], 0.2f);
+            transform.Rotate(Vector3.forward * Time.deltaTime * 60);
         }
-        //Debug.Log(currentTab);
     }
 
     public void ShiftLeft() {
         //decrement by 1 
-        if (currentTab > 0 && currentTab <= tabGroup.tabButtons.Count) {
+        int prevTab = currentTab;
+        if (currentTab > 0 && currentTab < tabs) {
             currentTab--;
+            //transform.rotation = Quaternion.Slerp(rotList[prevTab], rotList[currentTab], 0.2f);
+            transform.Rotate(-Vector3.forward * Time.deltaTime * 60);
         }
-       // Debug.Log(currentTab);
     }
-
-    public void Rotate() {
-        if (currentTab > 0 && currentTab < tabGroup.tabButtons.Count) {
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rotList[currentTab], 0.2f);
-        }
-        //Debug.Log(rotList[currentTab]);
-    }
-
-    /*
-    IEnumerator Hold() {
-        Debug.Log("Started Coroutine at timestamp : " + Time.time);
-        yield return new WaitForSeconds(2);
-        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
-    }
-    */
-   
-
 }
